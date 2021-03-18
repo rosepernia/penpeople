b91510cb4b0dec91653f1ec3ee58f890fb8b728f
Rosa <rosa.pernia75@gmail.com>
2021-03-18 14:06:10 +0100

    Primera vista

---
+++ src/views/Authors.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+<h1>Author</h1>
+</div>
+</template>
+<script>
+
+export default {
+  name: 'Author',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
