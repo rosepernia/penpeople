bb0e0314df7e5925fc1ee37d21f1191cd4747136
Rosa <rosa.pernia75@gmail.com>
2021-03-25 12:22:11 +0100

    Actualizado navegador

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,7 @@
 <template>
-  <div id="nav">
+  <div>
     <router-link to="/">Home</router-link> |
-    <router-link to="/autores">Autores</router-link> |
-    <router-link to="/nuevo-fragmento">Nuevo Fragmento</router-link> |
-    <router-link to="/nueva-historia">Nueva Historia</router-link> |
-    <router-link to="/lectura">Leer</router-link> |
-    <router-link to="/registro">Registro</router-link> |
-    <router-link to="/libros">Libros</router-link> |
-    <router-link to="/admin">Superuser</router-link> |
-    <router-link to="/perfil">Ver perfil</router-link> |
-    <router-link to="/moderar">Elegir cierre</router-link> 
-
-  
+    <router-link to="/autores">Autores</router-link> 
   </div>
   <router-view/>
 </template>
@@ -25,16 +15,4 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
