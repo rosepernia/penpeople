1fe5120bbde5a78313096e325daf61b3bfa8d40e
martasanch <marta.sanch.ger@gmail.com>
2021-04-16 11:33:57 +0200

    Creado componente y vista de authors, con estilo y funcionando

---
+++ src/components/AuthorBox.vue
@@ -0,0 +1,88 @@
+<template>
+
+  <div class="author box">
+    <div class="avatar">
+      <img v-bind:src="avatar">
+    </div>
+    <div class="description">
+      <h2 class="nickname" v-text="`@${nickname}`"></h2>
+      <div class="likes">
+        <i class="bi bi-heart-fill fa-3x fa-lg" style="color:red;"></i>
+        <h2 v-text="likes"></h2>
+      </div>
+      <router-link to="/UserProfile" class="button" href="">Ver usuario</router-link>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'AuthorBox',
+  components: {
+  },
+  props: {
+    avatar:String,
+    email:String,
+    nickname: String,
+    likes:Number
+  },
+  setup() {
+   
+  },
+}
+</script>
+
+<style lang="scss">
+
+  .author{
+    display:flex;
+    width: 350px;
+    margin: 20px;
+      .avatar{
+      /* background-color: red; */
+
+        img{
+          margin-top: 15px;
+          margin-right: 15px;
+          width: 120px;
+          height: 120px;
+          object-fit: cover;
+          object-position: center;
+          border-radius: 100%;
+          box-shadow: 1px 1px 3px #cfcdcd;
+        }
+      }
+      .description{
+        /* background-color: green; */
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 80%;
+        justify-content: center;
+        align-content: center;
+          h2{
+            margin:0px;
+          }
+          .likes{
+            display: inline-flex;
+            margin-bottom: 10px;
+            i{
+              padding-top:2px;
+              margin:0 10px; 
+              font-size: 23px;
+            }
+           /*  h2{
+              background-color: yellow;
+            }
+             */
+          }
+    
+      }
+
+   
+  }
+  
+
+</style>
--- src/views/Authors.vue
+++ src/views/Authors.vue
@@ -1,26 +1,69 @@
 <template>
-<div class="view-top">
-  <h1>Estamos en Autores</h1>
+<div class="view-top authors">
+ 
+  <AuthorBox v-for="(user,ind) in users" :key="ind"
+    :avatar="user.avatar"
+    :email="user.email"
+    :nickname="user.nickname"
+    :likes="user.likes"
+    />
+ 
 </div>
 </template>
 
 <script>
-
+import AuthorBox from "@/components/AuthorBox";
+import { ref, reactive, computed, onMounted } from 'vue'
 export default {
   name: "Author",
   components: {
-   
+    AuthorBox
   },
+  setup(props) {
 
-  setup() {
-    return {
-        
+    let users=reactive([])
+     
+    onMounted(() => {
+        list()
+        })
+
+    function list(){
+      fetch('http://localhost:8081/users/list',{
+        method: 'POST',
+        body: JSON.stringify({
+          "nickname":"",
+          "admin":"false"
+        }),
+        headers: {'Content-Type':'application/json'}
+      })
+      .then(resp=>resp.json())
+      .then(data=>{
+        console.log(data)
+        users.splice(0)
+        data.forEach(user => {
+        users.push(user)
+        })
+      })            
+    }
+    console.log(users)
+    return { users  
     }
   },
 }
+
+
 </script>
 
 <style lang="scss" scoped>
 
+.authors{
+  margin-right: auto;
+  margin-left: auto ;
+  display: flex;
+  flex-wrap: wrap;
+  width: 80%;
+  justify-content: space-evenly;
+  
+}
 
 </style>
