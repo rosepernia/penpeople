b5db00c429c891e6d7b896d4ebf9a60147591df9
DvM94 <danielvillarmartin@gmail.com>
2021-04-21 21:56:41 +0200

    Perfil super admin acabado

--- src/views/SuperUser.vue
+++ src/views/SuperUser.vue
@@ -3,15 +3,15 @@
     <div class="profile-form">
       <div class="title">Formulario de registro</div>
       <div class="box">
-      <input type="text" v-model="nickname" placeholder="Nickname">
+      <input type="text" v-model="form.nickname" placeholder="Nickname">
       <div class="error"><p v-if="error.value">{{error.value.nickname}}{{error.value.repeatnickname}}</p></div>
-      <input type="text" v-model="email" placeholder="Correo electrónico">
+      <input type="text" v-model="form.email" placeholder="Correo electrónico">
       <div class="error"><p v-if="error.value">{{error.value.email}}{{error.value.repeatemail}}</p></div>
-      <input type="text" v-model="firstname" placeholder="Nombre">
+      <input type="text" v-model="form.firstname" placeholder="Nombre">
       <div class="error"><p v-if="error.value">{{error.value.firstname}}</p></div>
-      <input type="text" v-model="lastname" placeholder="Apellidos">
+      <input type="text" v-model="form.lastname" placeholder="Apellidos">
       <div class="error"><p v-if="error.value">{{error.value.lastname}}</p></div>
-      <input type="password" v-model="password" placeholder="Contraseña">
+      <input type="password" v-model="form.password" placeholder="Contraseña">
       <div class="error"><p v-if="error.value">{{error.value.password}}</p></div>
       <div class="ok"><p v-if="oksignup"> Administrador registrado con éxito.</p></div>
       <button class="button" @click="createAdmin">Registrar Administrador</button>
@@ -22,46 +22,58 @@
         <p>Administradores</p>
         <div><i class="bi bi-search"></i><input type="text" v-model="letters" @keyup="inputLetter" placeholder="Nickname"></div>
       </div>
+      <div class="admins"> 
+        <UserCard v-for="(user,ind) in users" :key="ind" @update="update"
+          :avatar = "user.avatar"
+          :email = "user.email"
+          :nickname = "user.nickname"
+          user = false
+          />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { ref, reactive, watch, onMounted } from "vue";
+import UserCard from "@/components/UserCard"
+import { ref, reactive, onMounted } from "vue"
 import { useStore } from "vuex"
 
 export default {
   name: "SuperUser",
-  components: {},
-
+  components: {
+    UserCard
+  },
   setup() {
     const store = useStore()
-    const nickname = ref("")
-    const email = ref("")
-    const firstname = ref("")
-    const lastname = ref("")
-    const password = ref("")
+    const form = reactive({
+      nickname: "",
+      email: "",
+      firstname: "",
+      lastname: "",
+      password: ""
+    })
+    const oksignup = ref(false)
     const error = reactive({})
     const letters = ref("")
     const users = reactive([])
-    const oksignup = ref(false)
     const superadmin = ref(false)
 
     const createAdmin = () => {
       fetch('http://localhost:8081/users/create',{
         method: 'POST',
-        body: JSON.stringify({ nickname: nickname.value, email: email.value, firstname: firstname.value, lastname: lastname.value, password: password.value, admin: true, active: true }),
+        body: JSON.stringify({ nickname: form.nickname, email: form.email, firstname: form.firstname, lastname: form.lastname, password: form.password, admin: true, active: true }),
         headers: {'Content-Type':'application/json'}
       })
         .then(resp => resp.json())
         .then (data => {
           if (data=='ok') {
             error.value = ""
-            nickname.value = ""
-            email.value = ""
-            firstname.value = ""
-            lastname.value = ""
-            password.value = ""
+            form.nickname = ""
+            form.email = ""
+            form.firstname = ""
+            form.lastname = ""
+            form.password = ""
             oksignup.value = true
           }
           else {
@@ -71,9 +83,7 @@ export default {
         })
     }
 
-    let inputLetter = () => {
-      list(letters)
-    }
+    let inputLetter = () => list(letters.value)
 
     const list = (letters) => {
       fetch('http://localhost:8081/users/list',{
@@ -87,30 +97,28 @@ export default {
         .then(resp=>resp.json())
         .then(data=>{
           users.splice(0)
-          data.forEach(user => {
-          users.push(user)
-          })
+          const admins = data.filter(admin => admin.nickname!="Administrador")
+          admins.forEach(user => users.push(user))
         })            
     }
 
+    const update = () => list("")
+
     onMounted(() => {
-      list(letters)
+      list(letters.value)
       superadmin.value = store.state.user.superadmin
     })
 
     return {
-      nickname,
-      email,
-      firstname,
-      lastname,
-      password,
+      form,
+      oksignup,
       error,
       inputLetter,
       letters,
       users,
-      oksignup,
       createAdmin,
-      superadmin
+      superadmin,
+      update
     }
   },
 }
@@ -119,7 +127,7 @@ export default {
 <style lang="scss" scoped>
 .profile{
   width: 80%;
-  max-width: 900px;
+  max-width: 1100px;
   height: 500px;
   margin-bottom: 40px;
   display: flex;
@@ -147,6 +155,14 @@ export default {
         margin: 0 10px;
       }
     }
+    .admins{
+      height: 500px;
+      overflow: scroll;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      align-items: flex-start;
+    }
   }
 }
 input,textarea{
@@ -173,6 +189,9 @@ input{
     }
     &-admins{
       width: 100%;
+      .admins{
+      height: 300px;
+    }
     }
   }
 }
