b8ad6741f635e79c686a43b2d6d16c3964618792
DvM94 <danielvillarmartin@gmail.com>
2021-04-22 13:05:12 +0200

    Acabadas historias

--- src/views/ReadStory.vue
+++ src/views/ReadStory.vue
@@ -1,8 +1,7 @@
-<template> 
-  <div class="view-top block" v-if="story.value"> 
+<template>
+  <div class="view-top block" v-if="story.value">
     <div class="head">
-      <h2 class="head-title">{{story.value.title}}</h2> 
-      <i class="bi bi-diagram-3-fill head-tree"></i>
+      <h2 class="head-title">{{story.value.title}}<i class="bi bi-diagram-3-fill head-tree"></i></h2> 
     </div>
     <div class="box">
       <div><p class="box-title" v-if="block.value.author.avatar">{{choose}}</p></div> 
@@ -21,7 +20,6 @@
         </p>
       </div>
     </div>
-    
   </div>
 </template>
 
@@ -95,7 +93,7 @@ export default {
             for (let i = 0; i < block.value.closure.length; i++) {
               let decission = {title: block.value.closure[i], blockid: block.value.blockid}
               let blocksId = blocks.map(b=>b.blockid)
-              if(blocksId.indexOf(i.toString())!=-1) decission.active = true
+              if(blocksId.indexOf((block.value.blockid+i).toString())!=-1) decission.active = true
               else decission.active = false
               closures.push(decission)
             }
@@ -120,7 +118,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.view-top{
+  margin-top: 200px;
+}
 .block{
   width: 90%;
   max-width: 900px;
@@ -128,27 +128,39 @@ export default {
 }
 
 .head{
-  position: relative;
+  position: fixed;
+  top: 120px;
+  left: 0;
+  width: 100%;
+  background-color: #f4f1f1d7;
+  z-index: 10;
 }
 
 .head-title{
-   font-family: $font3;
-    margin-bottom: 10px ;
-    color: #52b1b9;
-    padding: 3px;
-    font-weight: bold;
-    font-size: $size2;
-    border-bottom:1px solid   #52b1b9;
+  position: relative;
+  width: 95%;
+  max-width: 1000px;
+  margin-right: auto;
+  margin-left: auto;
+  font-family: $font3;
+  margin-bottom: 10px ;
+  color: #52b1b9;
+  padding: 3px;
+  font-weight: bold;
+  font-size: $size2;
+  border-bottom:1px solid #52b1b9;
 }
 
 i.head-tree::before{
   top:0;
-  right:20px;
+  right:24px;
   position: absolute;
   font-size: $size4;
   color: #52b1b9;
 }
-
+.box{
+  margin-bottom: 40px;
+}
 .box-title{
   margin-bottom: 20px;
   padding: 0 0 0 30px;
